abe748427662b7d4bb818dca81fac8f61de7ca00
Peter Varley <petevarley@gmail.com>
2017-11-22 20:49:17 -0800

    Initial commit

---
+++ variables-practice.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+// This is a single-line comment.
+
+/* This is a really long multi-line comment that repeats itself so that it can fit across multiple lines. This is a really long multi-line comment that repeats itself so that it can fit across multiple lines.
+ */
+
+// Single line comments by Pete
+
+/* Multi-line comments by pete
+ This is a practice excersize
+ */
+
+/*: Multi-line comments by pete
+ This is a practice excersize
+ */
+
+//: Single line comments by Pete
+
+
+import UIKit
+
+var str = "Hello, playground" // This statement creates a variable.
+
